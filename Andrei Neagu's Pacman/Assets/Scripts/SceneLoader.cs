fcee2f20294c0f31492f9937684568e5698057be
Andrei Neagu <neaguandrei47@gmail.com>
2020-08-13 01:28:21 +0200

    main menu volume working

---
+++ Andrei Neagu's Pacman/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class SceneLoader : MonoBehaviour {
+
+public AudioMixer audioMixer;
+
+    public void LoadGame() {
+        SceneManager.LoadScene("game_scene");
+    }
+
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("MyExposedParam", volume);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
